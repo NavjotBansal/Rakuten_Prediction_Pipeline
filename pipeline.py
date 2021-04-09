6244cff1cb2e198ec9bae933b8c10caa35af8779
Navjot Bansal <navjotbansal09@gmail.com>
2021-04-09 12:11:20 +0530

    running pipeline

--- pipeline.py
+++ pipeline.py
@@ -7,8 +7,9 @@ import pandas as pd
 import numpy as np
 import tqdm.auto as tqdm
 import zipfile
+import cv2
 
-INPUT_FILE = 'kid_mixed.mp4'
+INPUT_FILE = 'input-zoom.mp4'
 FRAME_PATH = 'KID'
 
 CLASS_NAMES = ['Angry', 'Disgusted', 'Fear', 'Happy', 'Sad', 'Surprised', 'Neutral']
@@ -23,21 +24,10 @@ for modelname in modellist:
 if __name__ == '__main__':
 	feat_score_fold_0,gender_array,time_array = video_bounding_box.show_boxes(INPUT_FILE)
 	#print(feat_score_fold_0)
-	feat_score_fold_1 = list()
+	# feat_score_fold_1 = list()
 	print("Ensembling models")
-	vid_to_frame.video_to_frames(video_path=INPUT_FILE, frames_dir=FRAME_PATH, overwrite=False, every=30, chunk_size=1000)
-	for images in os.listdir(os.path.join(FRAME_PATH,INPUT_FILE)):
-		score, codes = frame_emotion_detection.predict_emotion(os.path.join(FRAME_PATH,INPUT_FILE),images)
-		#print(float(score),code)
-		_,dominantcode = torch.max(codes.data, 0)
-		dominantcode = dominantcode.cpu().numpy()
-		#print(score[dominantcode],CLASS_NAMES[dominantcode])
-		feat_score_fold_1.append(score.tolist())
-	#print(feat_score_fold_1)
-
-	feat_score_fold_1 = np.array(feat_score_fold_1)
 	feat_score_fold_0 = np.array(feat_score_fold_0)
-	aggregated_score = np.add(feat_score_fold_1,feat_score_fold_0)/2
+	aggregated_score = np.add(feat_score_fold_0,feat_score_fold_0)/2
 	row_sum = np.sum(aggregated_score,axis=1)
 	aggregated_score = (aggregated_score/row_sum[:,np.newaxis])
 	print("Creating aggregated_data")
